9ff03ee36ff1858eabee589c08fd531679f75932
Jose Ricardo Bernal León <joser.bernal@softtekprojects.com>
2024-05-15 12:37:00 -0500

    Corrected propertis files

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/repository/CustomerRepository.java
@@ -3,7 +3,9 @@ package com.paymentchain.customer.repository;
 import com.paymentchain.customer.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     @Query("SELECT c from Customer c WHERE c.code = ?1")
--- paymentchainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/repository/TransactionRepository.java
+++ paymentchainparent/businessdomain/transactions/src/main/java/com/paymentchain/transactions/repository/TransactionRepository.java
@@ -3,9 +3,10 @@ package com.paymentchain.transactions.repository;
 import com.paymentchain.transactions.entities.Transactions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface TransactionRepository extends JpaRepository<Transactions, Long> {
 
     @Query("SELECT t from Transactions t WHERE t.accountIban = ?1")
